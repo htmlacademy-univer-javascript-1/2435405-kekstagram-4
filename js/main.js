206f87f496500408849eb36b8b71ea2272a41796
Цепилов Александр <tsepilov.sasha@gmail.com>
2023-10-29 19:32:20 +0500

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,41 +1,62 @@
-const photos = [];
+const getRandomNumberFromInterval = (min, max)=> Math.floor(Math.random() * (max - min + 1) + min);
 
-const photoDescrpiptionsVariants =
-['Это моя семья',
+const photoCounts = 25;
+
+const DESCRIPTIONS = [
+  'Это моя семья',
   'Как вам?',
   'Оцените!',
   'Я с друзьями',
   'Что скажите?',
-  'Мы на прогулке'];
-const commentVariants =
-['Всё отлично!',
+  'Мы на прогулке'
+];
+const COMMENTS = [
+  'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const nameVariants =
-['Артём',
-  'Леша',
-  'Семен',
-  'Саша',
-  'Лиза',
-  'Даша',];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = ['Артём', 'Леша', 'Семен', 'Саша', 'Лиза', 'Даша',];
 
-const getRandomNumber = (min, max)=> Math.floor(Math.random() * (max - min + 1) + min);
+const avatarId = {min: 1, max: 6,};
 
-const getComments = () => {
-  const comments = [];
-  const commentsNumber = getRandomNumber(0,30);
-  for (let i = 1; i <= commentsNumber; i++) {
-    const comment = { id: i, avatar: `img/avatar-${getRandomNumber(1,6)}.svg`, message: commentVariants[getRandomNumber(0,5)], name: nameVariants[getRandomNumber(0,5)]};
-    comments.push(comment);
-  }
-  return comments;
-};
+const commentsCount = {min: 0, max: 30,};
 
-for (let i = 1; i <= 25; i++) {
-  const photo = { id: i, url: `photos/${i}.jpg`, description: photoDescrpiptionsVariants[getRandomNumber(0,5)], likes: getRandomNumber(15,200), coments: getComments()};
-  photos.push(photo);
-}
+const sentencesCount = {min: 1, max: 2,};
 
+const likesCount = {min: 15, max: 200,};
+
+const shuffle = (array) => array.sort(() => Math.random() - 0.5);
+
+const getComment = (_, id) => ({
+  id,
+  avatar: `img/avatar-${getRandomNumberFromInterval(
+    avatarId.min,
+    avatarId.max
+  )}.svg`,
+  message: shuffle(COMMENTS).slice(0, getRandomNumberFromInterval(
+    sentencesCount.min,
+    sentencesCount.max
+  )),
+  name: NAMES[getRandomNumberFromInterval(0, NAMES.length - 1)],
+});
+
+const getPhotoData = (_, id) => ({
+  id,
+  url: `photos/${id}.jpg`,
+  description: DESCRIPTIONS[getRandomNumberFromInterval(0, DESCRIPTIONS.length - 1)],
+  likes: getRandomNumberFromInterval(
+    likesCount.min,
+    likesCount.max
+  ),
+  coments: Array.from({length: getRandomNumberFromInterval(
+    commentsCount.min,
+    commentsCount.max
+  )}, (getComment)),
+});
+
+const getPhotos = () => Array.from({length: photoCounts}, getPhotoData);
+
+getPhotos();
